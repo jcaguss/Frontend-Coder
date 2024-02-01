95d0069a9d76d3b5cfc9f15705e3cd257e725669
Agustin Caceres <agustincaceresm@gmail.com>
2024-02-01 18:22:13 -0300

    Frontend

--- e-commerce/backend/src/routes/mockingProducts.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { postProducts } from "../controllers/mockingProducts.controller.js";
-
-const mockingProductsRouter = Router()
-
-mockingProductsRouter.post('/', postProducts)
-
-export default mockingProductsRouter
--- e-commerce/backend/src/utils/bcrypt.js
+++
@@ -1,5 +0,0 @@
-import bcript from 'bcrypt'
-
-export const createHash = (password) => bcript.hashSync(password, bcript.genSaltSync(parseInt(process.env.SALT)))
-
-export const validatePassword = (passwordSend, passwordBBD) => bcript.compareSync(passwordSend, passwordBBD)
