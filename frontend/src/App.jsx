caccc7eca618285af12bbb179bbc117e2cca81f9
Agustin Caceres <agustincaceresm@gmail.com>
2024-02-01 18:53:03 -0300

    error-de-extencion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Register} from './components/Register';
-import { Login } from './components/login';
-import { Products } from './components/Products';
-import { NewProducts } from './components/NewProducts';
+import {Register} from './components/Register.jsx';
+import { Login } from './components/login.jsx';
+import { Products } from './components/Products.jsx';
+import { NewProducts } from './components/NewProducts.jsx';
 
 export const App = () => {
   return (
