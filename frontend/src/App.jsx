2cf3ca417dd1a35cccc073e9f8d73da7416e78dc
Agustin Caceres <agustincaceresm@gmail.com>
2024-02-01 19:10:36 -0300

    prueba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {Register} from './components/Register.jsx';
-import { Login } from './components/login.jsx';
+import { Login } from './components/Logins.jsx';
 import { Products } from './components/Products.jsx';
 import { NewProducts } from './components/NewProducts.jsx';
 
